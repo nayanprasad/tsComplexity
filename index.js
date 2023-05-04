97704422891ef9f94f0705707fc1e0e1c8a07c00
Nayan Prasad P k <nayanprasad096@gmail.com>
2023-05-05 02:56:04 +0530

    handled response and error

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const performance = require('performance-now');
-const { Configuration, OpenAIApi } = require("openai");
+const {Configuration, OpenAIApi} = require("openai");
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 
 const configuration = new Configuration({
-  organization: "org-FFGHGaY6dYhffKNzcbGtcRoL",
-  apiKey: process.env.OPENAI_API_KEY ,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -17,14 +16,14 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.post("/getCode", async (req, res) => {
 
-    const {code, language} = req.body;
+    const {code} = req.body;
 
-    const prompt = `convert the following function  ${language} code into javascript \n ${code}`;
+    const prompt = `only give the time complexity of below function  ${code}`;
 
     const parameters = {
         "model": "gpt-3.5-turbo",
@@ -32,27 +31,27 @@ app.post("/getCode", async (req, res) => {
         "temperature": 0.7
     }
 
-    // const response = await openai.createChatCompletion(parameters);
-    // console.log(response.data);
+    const response = await openai.createChatCompletion(parameters)
+        .then((response) => {
+                res.status(200).json({
+                    success: true,
+                    data: response.data
 
-    const t0 = performance();
-    // myFunction();
-    const t1 = performance();
-    const time = t1 - t0;
-    console.log(`Time taken: ${t1 - t0} milliseconds`);
-
-
-    res.status(200).json({
-        success: true,
-        time: time,
-    })
-
+                })
+            }
+        ).catch((err) => {
+                res.status(200).json({
+                    success: false,
+                    message: err.message
+                })
+            }
+        );
 });
 
 
 const port = 3001;
 app.listen(port, (req, res) => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
 
 
